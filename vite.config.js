112ea402b0c0d92cd73fcb513bb387d61a1b616d
dannyysaputra <84920467+dannyysaputra@users.noreply.github.com>
2024-12-23 19:42:14 +0700

    feat - pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestForPlugin = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: "DALL-E CLONE",
+    short_name: "DALL-E CLONE",
+    description: "Post generate image from openai",
+    icons: [
+      {
+        src: "pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png"
+      },
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable"
+      }
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait"
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
